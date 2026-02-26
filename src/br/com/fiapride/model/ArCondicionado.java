9badcd23378f465e1256bcb16e568b6e12105151
Sergio Mirabelo <segiomirabelo@gmail.com>
2026-02-25 23:34:28 -0300

    projeto fiapride

---
+++ src/br/com/fiapride/model/ArCondicionado.java
@@ -0,0 +1,8 @@
+package br.com.fiapride.model;
+public class ArCondicionado { // <-- Use o nome do SEU objeto
+    // As características que você pensou
+    public String tipo;
+    public double altura;
+    public int tempmax;
+
+}
